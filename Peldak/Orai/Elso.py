59d90f2d4e627ed5dc50069124907bca87b9e7f8
Attirobert <karobatti@gmail.com>
2023-04-28 11:36:16 +0200

    Valami

---
+++ Peldak/Orai/Elso.py
@@ -0,0 +1,24 @@
+import turtle
+ablak = turtle.Screen()
+Toll = turtle.Turtle()
+
+# 1. lépés
+Toll.forward(50)
+Toll.left(90)
+Toll.forward(30)
+# EOF 1. lépés
+
+Toll.forward(50)
+Toll.left(90)
+Toll.forward(30)
+
+Toll.forward(50)
+Toll.left(90)
+Toll.forward(30)
+# EOF 1. lépés
+
+Toll.forward(50)
+Toll.left(90)
+Toll.forward(30)
+
+ablak.mainloop()
